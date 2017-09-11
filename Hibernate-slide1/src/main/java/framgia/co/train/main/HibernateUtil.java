14f8bb84a706ce45f633c8d9846a9ebb917afd96
vuthitranvan <vu.thi.tran.van@framgia.com>
2017-09-11 11:34:37 +0700

    example of slide1 - hibernate

---
+++ Hibernate-slide1/src/main/java/framgia/co/train/main/HibernateUtil.java
@@ -0,0 +1,30 @@
+package framgia.co.train.main;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	@SuppressWarnings("unused")
+	private static SessionFactory factory;
+
+	public static SessionFactory getFactory() {
+		return factory;
+	}
+
+	public static void setFactory(SessionFactory factory) {
+		HibernateUtil.factory = factory;
+	}
+
+	public static void config() {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			factory = configuration.buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+}
